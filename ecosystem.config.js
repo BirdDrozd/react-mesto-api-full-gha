f93d7f9fd207e51277a8ce70a5d6c824890b45d1
Vlad Drozdov <drozdov.vl.1@yandex.ru>
2023-10-08 21:01:25 +0400

    fix:PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [
+    {
+      name   : "backend",
+      script : "./backend/app.js",
+      env: {
+          PORT: "3000"
+      }
+    },
+    {
+      name   : "frontend",
+      script : "./frontend/node_modules/react-scripts/scripts/start.js",
+      env: {
+        PORT: 3001
+      }
+    }
+  ]
+}

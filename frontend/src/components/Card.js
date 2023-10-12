085e12317a36992da4addfeb787427b234ae88db
Vlad Drozdov <drozdov.vl.1@yandex.ru>
2023-10-12 14:44:59 +0400

    fix:card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import { useContext } from "react";
 
 function Card({ card, onCardClick, onDeleteClick, onCardLike }) {
   const currentUser = useContext(CurrentUserContext);
-  const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
   const isLiked = card.likes.some((i) => i._id === currentUser._id);
   const cardLikeButtonClassName = `cards__like ${
     (isLiked && "cards__like_active") || ""

39389def8236efa63e323ecd81e0b76403182ef7
Vlad Drozdov <drozdov.vl.1@yandex.ru>
2023-10-12 14:54:56 +0400

    fix:app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -114,7 +114,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
 
     configApi
       .changeLikeCardStatus(card._id, !isLiked)

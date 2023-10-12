9fb190e7ea88f04b9293cd5e4ede7cb16bbf4cb2
Vlad Drozdov <drozdov.vl.1@yandex.ru>
2023-10-12 14:36:04 +0400

    fix: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -9,7 +9,7 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = 'access-control-request-headers, Content-Type';
+  const requestHeaders = 'authorization, access-control-request-headers, Content-Type';
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   res.header('Access-Control-Allow-Credentials', true);
